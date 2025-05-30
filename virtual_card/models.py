a1569028d4fcfa74ddcb084698c42fc67254a437
Dibuji <emekzy10@gmail.com>
2025-05-30 20:04:55 +0100

    fix: added amount field in models.py and fixed urls for windows

--- virtual_card/models.py
+++ virtual_card/models.py
@@ -6,6 +6,7 @@ class VirtualCard(models.Model):
     bitnob_card_id = models.CharField(max_length=100, unique=True)
     card_brand = models.CharField(max_length=50, blank=True)
     card_type = models.CharField(max_length=50, blank=True)
+    amount = models.DecimalField(max_digits=18, decimal_places=2, default=0.00)
     status = models.CharField(max_length=50, default="pending")
     reference = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,4 +14,3 @@ class VirtualCard(models.Model):
 
     def __str__(self):
         return f"{self.card_brand} {self.bitnob_card_id} ({self.status})"
-
--- virtual_card/urls.py
+++ virtual_card/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateVirtualCardView, FundVirtualCardView
 
 urlpatterns = [
-    path("virtualcards/create", CreateVirtualCardView.as_view(), name="create-card"),
-    path('virtualcards/topup', FundVirtualCardView.as_view(), name='topup-virtual-card'),
+    path("virtualcards/create/", CreateVirtualCardView.as_view(), name="create-card"),
+    path('virtualcards/topup/', FundVirtualCardView.as_view(), name='topup-virtual-card'),
 ]
